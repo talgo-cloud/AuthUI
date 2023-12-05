5d57b4ae121067a09e0c211f4fbcc2ab260f3c8e
Gabe Farrell <gabe@mnrva.dev>
2023-12-05 05:23:44 +0000

    Very basic initial UI implementation

---
+++ src/components/GitHubLogin.vue
@@ -0,0 +1,123 @@
+<template>
+    <div class="soc-wrapper">
+    <button class="gsi-material-button">
+      <div class="gsi-material-button-state"></div>
+      <div class="gsi-material-button-content-wrapper">
+        <div class="gsi-material-button-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="mr-2" viewBox="0 0 1792 1792">
+                <path d="M896 128q209 0 385.5 103t279.5 279.5 103 385.5q0 251-146.5 451.5t-378.5 277.5q-27 5-40-7t-13-30q0-3 .5-76.5t.5-134.5q0-97-52-142 57-6 102.5-18t94-39 81-66.5 53-105 20.5-150.5q0-119-79-206 37-91-8-204-28-9-81 11t-92 44l-38 24q-93-26-192-26t-192 26q-16-11-42.5-27t-83.5-38.5-85-13.5q-45 113-8 204-79 87-79 206 0 85 20.5 150t52.5 105 80.5 67 94 39 102.5 18q-39 36-49 103-21 10-45 15t-57 5-65.5-21.5-55.5-62.5q-19-32-48.5-52t-49.5-24l-20-3q-21 0-29 4.5t-5 11.5 9 14 13 12l7 5q22 10 43.5 38t31.5 51l10 23q13 38 44 61.5t67 30 69.5 7 55.5-3.5l23-4q0 38 .5 88.5t.5 54.5q0 18-13 30t-40 7q-232-77-378.5-277.5t-146.5-451.5q0-209 103-385.5t279.5-279.5 385.5-103zm-477 1103q3-7-7-12-10-3-13 2-3 7 7 12 9 6 13-2zm31 34q7-5-2-16-10-9-16-3-7 5 2 16 10 10 16 3zm30 45q9-7 0-19-8-13-17-6-9 5 0 18t17 7zm42 42q8-8-4-19-12-12-20-3-9 8 4 19 12 12 20 3zm57 25q3-11-13-16-15-4-19 7t13 15q15 6 19-6zm63 5q0-13-17-11-16 0-16 11 0 13 17 11 16 0 16-11zm58-10q-2-11-18-9-16 3-14 15t18 8 14-14z"></path>
+            </svg>
+        </div>
+        <span class="gsi-material-button-contents">Sign in with GitHub</span>
+        <span style="display: none;">Sign in with GitHub</span>
+      </div>
+    </button>
+    </div>
+    </template>
+    
+    <style scoped>
+    .soc-wrapper {
+        width: 100%;
+    }
+    
+    .gsi-material-button {
+      display: block;
+      margin: 0px auto;
+      -moz-user-select: none;
+      -webkit-user-select: none;
+      -ms-user-select: none;
+      -webkit-appearance: none;
+      background-color: WHITE;
+      background-image: none;
+      border: 1px solid var(--grey-light);
+      -webkit-border-radius: 5px;
+      border-radius: 5px;
+      -webkit-box-sizing: border-box;
+      box-sizing: border-box;
+      color: #1f1f1f;
+      cursor: pointer;
+      font-family: 'Roboto', arial, sans-serif;
+      font-size: 14px;
+      height: 46px;
+      letter-spacing: 0.25px;
+      outline: none;
+      overflow: hidden;
+      padding: 0 12px;
+      position: relative;
+      text-align: center;
+      -webkit-transition: background-color .218s, border-color .218s, box-shadow .218s;
+      transition: background-color .218s, border-color .218s, box-shadow .218s;
+      vertical-align: middle;
+      white-space: nowrap;
+      width: auto;
+      width: 240px;
+      min-width: min-content;
+    }
+    
+    .gsi-material-button .gsi-material-button-icon {
+      height: 20px;
+      margin-right: 12px;
+      min-width: 20px;
+      width: 20px;
+    }
+    
+    .gsi-material-button .gsi-material-button-content-wrapper {
+      -webkit-align-items: center;
+      align-items: center;
+      display: flex;
+      -webkit-flex-direction: row;
+      flex-direction: row;
+      -webkit-flex-wrap: nowrap;
+      flex-wrap: nowrap;
+      height: 100%;
+      justify-content: space-between;
+      position: relative;
+      width: 100%;
+    }
+    
+    .gsi-material-button .gsi-material-button-contents {
+      -webkit-flex-grow: 1;
+      flex-grow: 1;
+      font-family: 'Roboto', arial, sans-serif;
+      font-weight: 500;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      vertical-align: top;
+    }
+    
+    .gsi-material-button .gsi-material-button-state {
+      -webkit-transition: opacity .218s;
+      transition: opacity .218s;
+      bottom: 0;
+      left: 0;
+      opacity: 0;
+      position: absolute;
+      right: 0;
+      top: 0;
+    }
+    
+    .gsi-material-button:disabled {
+      cursor: default;
+      background-color: #ffffff61;
+      border-color: #1f1f1f1f;
+    }
+    
+    .gsi-material-button:disabled .gsi-material-button-contents {
+      opacity: 38%;
+    }
+    
+    .gsi-material-button:disabled .gsi-material-button-icon {
+      opacity: 38%;
+    }
+    
+    .gsi-material-button:not(:disabled):active .gsi-material-button-state, 
+    .gsi-material-button:not(:disabled):focus .gsi-material-button-state {
+      background-color: #303030;
+      opacity: 12%;
+    }
+    .gsi-material-button:not(:disabled):hover .gsi-material-button-state {
+      background-color: #303030;
+      opacity: 8%;
+    }
+    
+    </style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,155 @@
+<script setup lang="ts">
+import GoogleLogin from '../components/GoogleLogin.vue'
+import GitHubLogin from '../components/GitHubLogin.vue'
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="160"/>
+      <div class="login">
+        <div class="socials">
+          <GoogleLogin />
+          <GitHubLogin />
+        </div>
+        <p class="separat-or"><span>or</span></p>
+        <input type="email" name="tlg_email" id="tlg_email" placeholder="Email address">
+        <input type="password" name="tlg_password" id="tlg_password" placeholder="Password">
+        <button type="submit">Sign In</button>
+        <div class="remember">
+          <label for="tlg_remember" class="chk-container">Remember me for 30 days
+            <input type="checkbox" name="tlg_remember" id="tlg_remember">
+            <span class="checkmark"></span>
+          </label>
+        </div>
+      </div>
+    </div>
+    <div class="view-switch">Don't have an account? <RouterLink class="rlink" to="/register">Create an account.</RouterLink></div>
+  </div>
+</template>
+
+<style>
+.view-switch {
+  text-align: center;
+  margin-top: 4px;
+}
+.rlink {
+  text-decoration: none;
+  color: var(--purple);
+  font-weight: 700;
+}
+.rlink:hover {
+  color: var(--purple-light);
+}
+.logo {
+  display: block;
+  margin: 1em auto 0em auto;
+}
+.container {
+  padding: 2em 4em;
+  background-color: var(--white-soft);
+  border-radius: 5px;
+  max-width: 440px;
+  filter: drop-shadow(3px 6px 6px var(--color-drop-shadow));
+  margin: auto;
+  margin-top: 6vh;
+}
+.login {
+  display: flex;
+  flex-direction: column;
+}
+.socials {
+  margin: 3em 0em;
+}
+.socials > * {
+  margin-bottom: 0.5em;
+}
+.separat-or {
+  background-color: var(--white-soft);
+  text-align: center;
+  margin: 0px auto;
+  margin-bottom: 3em;
+  width: 90%;
+  line-height: 0.1em;
+  border-bottom: 2px solid var(--grey-light); 
+}
+.separat-or span {
+  background: var(--white-soft); 
+  padding:0 10px; 
+}
+.remember {
+  margin: 1em;
+  margin-bottom: 2em;
+  position: relative;
+}
+
+/* custom checkmark code from https://www.w3schools.com/howto/howto_css_custom_checkbox.asp */
+
+/* Customize the label (the container) */
+.chk-container {
+  display: block;
+  position: relative;
+  padding-left: 25px;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Hide the browser's default checkbox */
+.chk-container input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+/* Create a custom checkbox */
+.checkmark {
+  position: absolute;
+  top: 2px;
+  left: 0;
+  height: 18px;
+  width: 18px;
+  border-radius: 3px;
+  background-color: #ddd;
+}
+
+/* On mouse-over, add a grey background color */
+.chk-container:hover input ~ .checkmark {
+  background-color: #bbb;
+}
+
+/* When the checkbox is checked, add a blue background */
+.chk-container input:checked ~ .checkmark {
+  background-color: var(--purple);
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the checkmark when checked */
+.chk-container input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the checkmark/indicator */
+.chk-container .checkmark:after {
+  left: 7px;
+  top: 3px;
+  width: 5px;
+  height: 10px;
+  border: solid white;
+  border-width: 0 2px 2px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+</style>
